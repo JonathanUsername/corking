aece34d6a8462cc9fe25d3a1527e33045b8f9bd1
Marc Coury <gloriphobia@gmail.com>
2014-10-15 00:09:44 +0100

    made app.py output the json, commiting in order to test it on the server

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ app.config['DEBUG'] = True
 # Temporary assumptions (to be updated when we can get a json file from the front end).
 screen_width=400
 screen_height=400
+no_tiles=1600
 
+def generate_desert(size):
+	return [30 for i in range(size)]
 
 def check_name(newname):
 	# Later on add a database access to see if the name is already taken
@@ -27,9 +30,49 @@ def new_name():
 
 @app.route("/newgame")
 def give_object_coordinates():
-	js = [ { "rocks" : [[10,134],[234,122]],
-		 "base" : [midpoint(screen_width),midpoint(screen_height)] } ]
+	js = [ { "height":40,
+ 		"layers":[
+        {
+		 "data" : generate_desert(no_tiles),
+		 "height":40,
+         "name":"Ground",
+         "opacity":1,
+         "type":"tilelayer",
+         "visible":True,
+         "width":40,
+         "x":0,
+         "y":0
+        }],"orientation":"orthogonal",
+		"properties":
+			{
+		 	},
+		"tileheight":32,
+		"tilesets":[
+		    	{
+		        "firstgid":1,
+		        "image":"~/corking/tmw_desert_spacing.png",
+		        "imageheight":199,
+		        "imagewidth":265,
+		        "margin":1,
+		        "name":"Desert",
+		        "properties":
+		        	{
+		 		    },
+		        "spacing":1,
+		        "tileheight":32,
+		        "tilewidth":32
+		        }],
+		"tilewidth":32,
+		"version":1,
+		"width":40
+		} ]
 	return Response(json.dumps(js), mimetype='application/json')
+
+
+
+# @app.route("/test")
+# def test():
+# 	return Response(json.dumps(generate_desert(1600)),mimetype='application/json')
 
 if __name__ == "__main__":
     app.run()
