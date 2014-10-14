a810d562d90a948135ce1a928e9253ac9d017946
Marc Coury <marc.coury07@imperial.ac.uk>
2014-10-14 22:30:56 +0100

    app.py currently returns the name given to it after the newname director

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
-from flask import Flask
+from flask import Flask, request, abort, Response, jsonify, make_response, current_app, g
+import ipdb
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def check_name(newname):
+	# Later on add a database access to see if the name is already taken
+	# Assuming that there is no problem:
+	return newname
+
+@app.route("/newname")
+def new_name():
+#	ipdb.set_trace()
+	name = request.url.split("?")[1]
+	return name
 
 if __name__ == "__main__":
     app.run()
