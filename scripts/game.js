59addeeed24eaa91b0891a9a545fe3c44d4c39f2
Jon@home <jon@5app.co.uk>
2014-11-23 21:13:51 +0000

    created end turn functionality

--- scripts/game.js
+++ scripts/game.js
@@ -36,6 +36,36 @@ require(['Phaser', 'jquery'], function(Phaser, $) {
 
     get_new_game();
 
+    end_turn = function(mapdata){
+        var obj = []
+        for (var i in mapdata){
+            for (var j in mapdata[i]){
+                // Get rid of circular properties
+                mapdata[i][j].layer = null
+                mapdata[i][j].collisionCallbackContext = null
+                obj.push(mapdata[i][j])
+            }
+        }
+        var jsonSave = JSON.stringify(obj)
+        $.ajax({
+            url: "/endturn",
+            type: "POST",
+            data: jsonSave,
+            success: function(data){
+                loaded_game = data;
+                console.log(JSON.stringify(data))
+                try {
+                    game.destroy()
+                    console.log("Loading new game")
+                }
+                catch(e) {
+                    console.log("Game starting")
+                }
+                start_game(data)
+            }
+        })
+    }
+
     start_game = function(loaded){
         game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, '', {
             preload: preload,
@@ -71,29 +101,45 @@ require(['Phaser', 'jquery'], function(Phaser, $) {
             restart_key.onDown.add(function(){
                 get_new_game();
             }, this);
+            end_turn_key = game.input.keyboard.addKey(Phaser.Keyboard.E);
+            end_turn_key.onDown.add(function(){
+                mapdata = map.layers[0].data
+                end_turn(mapdata);
+            }, this);
         }
 
         function update() {
             marker.x = layer.getTileX(game.input.activePointer.worldX) * 32;
             marker.y = layer.getTileY(game.input.activePointer.worldY) * 32;
             if (game.input.mousePointer.isDown) {
-                // Holding shift
-                if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-                    console.log("painting solar")
-                    map.putTile(solar_panel, layer.getTileX(marker.x), layer.getTileY(marker.y))
-                    // currentTile = map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y));
-                    // console.log(currentTile)
-                } else if (game.input.keyboard.isDown(Phaser.Keyboard.R)) {
-                    console.log("painting residence")
-                    map.putTile(residence, layer.getTileX(marker.x), layer.getTileY(marker.y))
-                    // currentTile = map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y));
-                    // console.log(currentTile)
+                var xt = layer.getTileX(marker.x)
+                var yt = layer.getTileY(marker.y)
+                currentTile = map.getTile(xt, yt);
+                console.log(currentTile)
+                if (!currentTile.properties.built){
+                    // Holding shift
+                    if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+                        console.log("painting solar")
+                        map.putTile(solar_panel, xt, yt)
+                        currentTile.properties.built = true;
+                        // currentTile = map.getTile(xt, yt);
+                        // console.log(currentTile)
+                    } else if (game.input.keyboard.isDown(Phaser.Keyboard.R)) {
+                        console.log("painting residence")
+                        map.putTile(residence, xt, yt)
+                        currentTile.properties.built = true;
+                        // currentTile = map.getTile(xt, yt);
+                        // console.log(currentTile)
+                    } else {
+                        // Just clicking
+                        map.putTile(desert, xt, yt)
+                        // if (map.getTile(xt, yt) != currentTile) {
+                        //     map.putTile(currentTile, layer.getTileX(marker.x), layer.getTileY(marker.y))
+                        // }
+                    }
                 } else {
-                    // Just clicking
-                    map.putTile(desert, layer.getTileX(marker.x), layer.getTileY(marker.y))
-                    // if (map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y)) != currentTile) {
-                    //     map.putTile(currentTile, layer.getTileX(marker.x), layer.getTileY(marker.y))
-                    // }
+                    marker.lineStyle(2, 0xffffff, 1);
+                    console.log("You can't build on that! There's already a building there!")
                 }
             }
 
