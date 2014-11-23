2d64cb87bf5f6f34bb33d9d5fcce0645e8a7c32c
Thomas Jennings <t@Thomas-Jenningss-MacBook.local>
2014-11-23 21:47:10 +0000

    Changed generate desert to make half the map DESERT and half SOLAR

--- hello.py
+++ hello.py
@@ -13,73 +13,86 @@ app.config['DEBUG'] = True
 screen_width=400
 screen_height=400
 no_tiles=1600
+DESERT = 30
+SOLAR = 22
+RESIDENT = 23
 
 def generate_desert(size):
-	return [int(math.ceil(48*random.random())) for i in range(size)]
+    desertmap = []
+    for i in range(size):
+        if( i < (size/2)): tile = DESERT
+        else: tile = SOLAR
+        desertmap.append(tile)
+    return desertmap
+            
+                
+            
+                    
 
 def check_name(newname):
-	# Later on add a database access to see if the name is already taken
-	# Assuming that there is no problem:
-	return newname
+    # Later on add a database access to see if the name is already taken
+    # Assuming that there is no problem:
+    return newname
 
 def midpoint(length):
-	return length/2
+    return length/2
 
 @app.route("/")
 def root():
-	return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/newname")
 def new_name():
-#	ipdb.set_trace()
-	name = request.url.split("?")[1]
-	return name
+#    ipdb.set_trace()
+    name = request.url.split("?")[1]
+    return name
 
 @app.route("/newgame")
 def give_object_coordinates():
-	js = { "height":40,
- 		"layers":[
-        {
-		 "data" : generate_desert(no_tiles),
-		 "height":40,
-         "name":"Ground",
-         "opacity":1,
-         "type":"tilelayer",
-         "visible":True,
-         "width":40,
-         "x":0,
-         "y":0
-        }],"orientation":"orthogonal",
-		"properties":
-			{
-		 	},
-		"tileheight":32,
-		"tilesets":[
-		    	{
-		        "firstgid":1,
-		        "image":"~/corking/tmw_desert_spacing.png",
-		        "imageheight":199,
-		        "imagewidth":265,
-		        "margin":1,
-		        "name":"Desert",
-		        "properties":
-		        	{
-		 		    },
-		        "spacing":1,
-		        "tileheight":32,
-		        "tilewidth":32
-		        }],
-		"tilewidth":32,
-		"version":1,
-		"width":40
-		}
-	return Response(json.dumps(js), mimetype='application/json')
+    js = { "height":40,
+         "layers":[
+                {
+         "data" : generate_desert(no_tiles),
+         "height":40,
+                 "name":"Ground",
+                 "opacity":1,
+                 "type":"tilelayer",
+                 "visible":True,
+                 "width":40,
+                 "x":0,
+                 "y":0
+                }],"orientation":"orthogonal",
+        "properties":
+            {
+             },
+        "tileheight":32,
+        "tilesets":[
+                    {
+                        "firstgid":1,
+                        "image":"~/corking/tmw_desert_spacing.png",
+                        "imageheight":199,
+                        "imagewidth":265,
+                        "margin":1,
+                        "name":"Desert",
+                        "properties":
+                            {
+                         },
+                        "spacing":1,
+                        "tileheight":32,
+                        "tilewidth":32
+                        }],
+        "tilewidth":32,
+        "version":1,
+        "width":40
+        }
+    return Response(json.dumps(js), mimetype='application/json')
 
 
 
 # @app.route("/test")
 # def test():
-# 	return Response(json.dumps(generate_desert(1600)),mimetype='application/json')
+#     return Response(json.dumps(generate_desert(1600)),mimetype='application/json')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"))
+        app.run(host="0.0.0.0", port=int("5000"))
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
