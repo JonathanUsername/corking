f31586c66e608b86330b7b318c5ea64ba286b103
Tom <tomwjennings@gmail.com>
2014-11-23 23:09:14 +0000

    Renamed generate_desert to get_map

--- hello.py
+++ hello.py
@@ -19,7 +19,7 @@ DESERT = 30
 SOLAR = 22
 RESIDENT = 23
 
-def generate_desert(size):
+def get_map(size):
     #ipdb.set_trace()
     desertmap = []
     for i in range(size):
@@ -64,7 +64,7 @@ def give_object_coordinates():
     js = { "height":HEIGHT,
          "layers":[
                 {
-         "data" : generate_desert(no_tiles),
+         "data" : get_map(no_tiles),
          "height":HEIGHT,
                  "name":"Ground",
                  "opacity":1,
@@ -103,7 +103,7 @@ def give_object_coordinates():
 
 # @app.route("/test")
 # def test():
-#     return Response(json.dumps(generate_desert(1600)),mimetype='application/json')
+#     return Response(json.dumps(get_map(1600)),mimetype='application/json')
 
 if __name__ == "__main__":
         app.run(host="0.0.0.0", port=int("5000"))
